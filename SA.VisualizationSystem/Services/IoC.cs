3b7193795412a664cd428bbfe4fd4bf15aeb5ea3
world666 <world666.kirillov@mail.ru>
2014-05-29 14:13:55 +0300

    kyrylov start

---
+++ SA.VisualizationSystem/Services/IoC.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using Ninject.Parameters;
+using SA.VisualizationSystem.ViewModel;
+
+namespace SA.VisualizationSystem.Services
+{
+    public static class IoC
+    {
+        static IoC()
+        {
+            SetBindings();
+        }
+
+        public static void RegisterSingleton<TInterface, TClass>() where TClass : TInterface
+        {
+            _kernel.Bind<TInterface>().To<TClass>().InSingletonScope();
+        }
+
+        public static void RegisterInstance<TInterface, TClass>() where TClass : TInterface
+        {
+            _kernel.Bind<TInterface>().To<TClass>();
+        }
+
+        public static T Resolve<T>(params IParameter[] parameters)
+        {
+            return _kernel.Get<T>(parameters);
+        }
+
+        private static void SetBindings()
+        {
+            IoC.RegisterSingleton<MainVm, MainVm>();
+        }
+
+        private static readonly IKernel _kernel = new StandardKernel();
+    }
+}
---
+++ SA.VisualizationSystem/ViewModel/MainVm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using SA.VisualizationSystem.Services;
+
+namespace SA.VisualizationSystem.ViewModel
+{
+    public class MainVm : ViewModelBase
+    {
+        public MainVm()
+        {
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                RaisePropertyChanged("CurrentView");
+            }
+        }
+
+        
+        public ICommand MenuClick
+        {
+            get { return _viewMenuCommand ?? (_viewMenuCommand = new RelayCommand<object>(MenuViewControl)); }
+        }
+
+        private void MenuViewControl(object t)
+        {
+            var menuStr = t as string;
+
+            if (menuStr.Equals("GeneralView", StringComparison.InvariantCulture))
+                CurrentView = IoC.Resolve<GeneralVm>();
+        }
+
+        private object _currentView;
+        private RelayCommand<object> _viewMenuCommand;
+    }
+}
